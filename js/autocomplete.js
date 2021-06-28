db8f0223391a6b297ef7296b9543c4977fa01881
Jan Bloch <jan.bloch@noseryoung.ch>
2021-06-28 09:30:53 +0200

    only show autocomplete on focus

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -12,18 +12,30 @@ const buildAutocomplete = (domElement, list, {limit}) => {
     });
     const wrapper = document.createElement('div');
     
-    domElement.addEventListener('input', (event) => {
-        console.log(event.target.value);
-        if(event.target.value != '') {
-            if(!event.target.parentNode.classList.contains('editing'))
-                event.target.parentNode.classList.add('editing');
-        }else{
-            event.target.parentNode.classList.remove('editing');
-        }
-    })
+    domElement.addEventListener('input', updateEditingClass);
+    domElement.addEventListener('focusout', removeEditingClassIfFocusLost([domElement.id, divElement.id]));
+    divElement.addEventListener('focusout', removeEditingClassIfFocusLost([domElement.id, divElement.id]));
+    domElement.addEventListener('focusin', updateEditingClass);
     domElement.parentNode.insertBefore(wrapper, domElement);
     wrapper.appendChild(domElement);
     wrapper.appendChild(divElement);
     wrapper.className="autocomplete-wrapper";
     divElement.className="autocomplete-list";
+    divElement.setAttribute('tabindex', 1);
 };
+
+const updateEditingClass = (event) => {
+    if(event.target.value != '') {
+        if(!event.target.parentNode.classList.contains('editing'))
+            event.target.parentNode.classList.add('editing');
+    }else{
+        event.target.parentNode.classList.remove('editing');
+    }
+}
+
+const removeEditingClassIfFocusLost = (validIds) => {
+    return ((event)=>{
+        if(!event.relatedTarget || !validIds.includes(event.relatedTarget.id))
+            event.target.parentNode.classList.remove('editing');
+    })
+}

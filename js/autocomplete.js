2511ed72a2cb19ff503b6b4fc39b8081678021ae
Jan Bloch <jan.bloch@noseryoung.ch>
2021-06-28 09:05:49 +0200

    only show autocomplete when user edits text field

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,6 +1,29 @@
 
-const buildAutocomplete = (domElement) => {
+const buildAutocomplete = (domElement, list, {limit}) => {
     if(!domElement instanceof HTMLElement)
         return;
     domElement.setAttribute('autocomplete', 'off');
+
+    const divElement = document.createElement('div');
+    list.forEach(v=>{
+        const childElement = document.createElement('div');
+        childElement.innerText = v;
+        divElement.appendChild(childElement);
+    });
+    const wrapper = document.createElement('div');
+    
+    domElement.addEventListener('input', (event) => {
+        console.log(event.target.value);
+        if(event.target.value != '') {
+            if(!event.target.parentNode.classList.contains('editing'))
+                event.target.parentNode.classList.add('editing');
+        }else{
+            event.target.parentNode.classList.remove('editing');
+        }
+    })
+    domElement.parentNode.insertBefore(wrapper, domElement);
+    wrapper.appendChild(domElement);
+    wrapper.appendChild(divElement);
+    wrapper.className="autocomplete-wrapper";
+    divElement.className="autocomplete-list";
 };
